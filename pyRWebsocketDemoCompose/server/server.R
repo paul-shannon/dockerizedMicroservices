59e357168271c89a679b0cfe9427a790de1bccf3
paul-shannon <paul.thurmond.shannon@gmail.com>
2017-11-04 14:45:15 -0700

    now works, updates upon changed source

--- pyRWebsocketDemoCompose/server/server.R
+++ pyRWebsocketDemoCompose/server/server.R
@@ -5,8 +5,8 @@ socket = init.socket(context,"ZMQ_REP")
 bind.socket(socket,"tcp://*:5557")
 while(TRUE) {
    request = receive.string(socket)
-   printf("received request: %s", request)
-   response <- toupper(request)
+   printf("server.R received REQUEST: %s-%s", request, "from server.R")
+   response <- sprintf("%s (from pyRWebSocketDemoCompose/server.R)", toupper(request))
    send.raw.string(socket, response)
    Sys.sleep(1)
    }
