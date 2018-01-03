6f76c6b936e4af0231ed6b90c754d29bb3a74283
paul-shannon <paul.thurmond.shannon@gmail.com>
2018-01-03 10:53:20 -0800

    now uncommented ~/.gitconfig

--- trenaBase/.Rprofile
+++ trenaBase/.Rprofile
@@ -2,5 +2,5 @@ printf = function (...) print (noquote (sprintf (...)))
 q <- function(...) quit("no")
 wdth <- function(x){options(width=x)}
 wdth(2048)
-.libPaths("/home/trena/library")
+.libPaths("~/library")
 biocGet <- function(...){source("http://bioconductor.org/biocLite.R"); biocLite(...)}
--- trenaBase/installTrenaPrequisites.R
+++ trenaBase/installTrenaPrequisites.R
@@ -1,4 +1,7 @@
 printf <- function(...) print(noquote(sprintf(...)))
+my.user.library <- "~/library"
+if(!my.user.library %in% .libPaths())
+    .libPaths(my.user.library)
 
 source("http://bioconductor.org/biocLite.R")
 
@@ -16,30 +19,31 @@ code.pkgs <- c("GenomicRanges",
                "vbsr",
                "stringr",
                "httpuv",
-               "jsonlite",
-               "rzmq",
                "colorspace",
                "annotate",
+               "MotifDb",
                "splitstackshape",
                "RUnit")
 
-biocLite(code.pkgs, lib="/home/trena/library")
+biocLite(code.pkgs, lib="~/library")
 
 data.pkgs <- c("BSgenome",
                "BSgenome.Hsapiens.UCSC.hg38",
                "BSgenome.Hsapiens.UCSC.hg19",
                "BSgenome.Mmusculus.UCSC.mm10",
                "org.Hs.eg.db",
-               "org.Mm.eg.db")
-#               "SNPlocs.Hsapiens.dbSNP150.GRCh38")
+               "org.Mm.eg.db",
+               "SNPlocs.Hsapiens.dbSNP150.GRCh38",
+               "BSgenome.Athaliana.TAIR.TAIR9"
+               )
 
 for(data.pkg in data.pkgs){
    suppressWarnings(
-      needed <- !require(data.pkg, character.only=TRUE, quiet=TRUE)
+      needed <- !require(data.pkg, character.only=TRUE, lib.loc="~/library", quiet=TRUE)
       )
    printf("%s needed? %s", data.pkg, needed)
    if(needed)
-      biocLite(data.pkg,quiet=FALSE, lib="/home/trena/library")
+      biocLite(data.pkg,quiet=FALSE, lib="~/library")
    } # for
 
 
